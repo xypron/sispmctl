c626fa6afd785cb505b43dc06961a54b2a3fa593
Heinrich Schuchardt <xypron.glpk@gmx.de>
2018-12-18 10:24:27 +0100

    extras/sispmapplet.py: remove training whitespace
    Remove trailing whitespace.
    Remove double empty lines.
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- extras/sispmapplet.py
+++ extras/sispmapplet.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 #  sispmapplet
-# 
+#
 #  Python GUI script to control the GEMBIRD Silver Shield PM USB outlet device
-# 
+#
 #  (C) 2006, Mondrian Nuessle, Computer Architecture Group, University of Mannheim, Germany
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -32,57 +32,57 @@ def socket_off(num):
 def socket_on(num):
     #print "turn socket",num,"on"
     os.system("/usr/local/bin/sispmctl -q -o "+str(num))
-    
+
 
 def socket_get_state(num):
     #print "get state of socket",num
     #fd=os.popen("/usr/local/bin/sispmctl -g "+str(num))
     result=0
     for line in os.popen("/usr/local/bin/sispmctl -q -n -g "+str(num)).readlines():
-        #print "line: ",line       
+        #print "line: ",line
         if line[0]=="0":
             result=1
         elif line[0]=="1":
             result=0
     return result
-    
-class Mysispmform(sispmform): 
-    
+
+class Mysispmform(sispmform):
+
     def socket1_clicked(self):
         #print "socket1 clicked"
         if self.s1==0:
             socket_off(1)
-            self.led1.setColor(QColor(255,0,0))        
+            self.led1.setColor(QColor(255,0,0))
         else:
             socket_on(1)
             self.led1.setColor(QColor(0,255,0))
         self.s1=1-self.s1
-    
+
     def socket2_clicked(self):
         #print "socket1 clicked"
         if self.s2==0:
             socket_off(2)
-            self.led2.setColor(QColor(255,0,0))        
+            self.led2.setColor(QColor(255,0,0))
         else:
             socket_on(2)
             self.led2.setColor(QColor(0,255,0))
         self.s2=1-self.s2
-    
+
     def socket3_clicked(self):
         #print "socket1 clicked"
         if self.s3==0:
             socket_off(3)
-            self.led1.setColor(QColor(255,0,0))        
+            self.led1.setColor(QColor(255,0,0))
         else:
             socket_on(3)
             self.led1.setColor(QColor(0,255,0))
         self.s3=1-self.s3
-    
+
     def socket4_clicked(self):
         #print "socket4 clicked"
         if self.s4==0:
             socket_off(4)
-            self.led4.setColor(QColor(255,0,0))        
+            self.led4.setColor(QColor(255,0,0))
         else:
             socket_on(4)
             self.led4.setColor(QColor(0,255,0))
@@ -94,7 +94,7 @@ class Mysispmform(sispmform):
         self.s2=socket_get_state(2)
         self.s3=socket_get_state(3)
         self.s4=socket_get_state(4)
-        
+
         if self.s1==1:
             self.led1.setColor(QColor(255,0,0))
         else:
@@ -111,15 +111,12 @@ class Mysispmform(sispmform):
             self.led4.setColor(QColor(255,0,0))
         else:
             self.led4.setColor(QColor(0,255,0))
-        
+
         self.connect(self.socket1, SIGNAL("clicked()"), self.socket1_clicked)
         self.connect(self.socket2, SIGNAL("clicked()"), self.socket2_clicked)
         self.connect(self.socket3, SIGNAL("clicked()"), self.socket3_clicked)
         self.connect(self.socket4, SIGNAL("clicked()"), self.socket4_clicked)
-        
-  
-        
-        
+
 if __name__ == "__main__":
         a = QApplication(sys.argv)
         QObject.connect(a,SIGNAL("lastWindowClosed()"),a,SLOT("quit()"))
